749b34520cf2943bb417d16437fadc071f16dc76
yliu41 <yliu41@yfve.com.cn>
2015-01-05 13:19:59 +0800

    Add git config and vim script

---
+++ git_config.sh
@@ -0,0 +1,10 @@
+git config --global user.name yliu41
+git config --global user.email yliu41@yfve.com.cn
+git config --global color auto
+git config --global color.status auto
+git config --global color.diff auto
+git config --global color.branch auto
+git config --global color.interactive auto
+git config --global http.proxy http://yliu41:Qiang%4026@136.17.0.23:83/
+git config --global https.proxy http://yliu41:Qiang%4026@136.17.0.23:83/
+
